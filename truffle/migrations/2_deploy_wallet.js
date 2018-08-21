e7d741389414a0f91417fac2ccf603567bb2e517
josephwu1994 <josephwu1994@gmail.com>
2018-08-21 14:51:10 -0700

    Added Wallet.sol and migration setting/truffle network setting.

---
+++ truffle/migrations/2_deploy_wallet.js
@@ -0,0 +1,10 @@
+const DefaultSweeper = artifacts.require('DefaultSweeper');
+const Token = artifacts.require('Token');
+const Controller = artifacts.require('Controller');
+
+module.exports = function(deployer) {
+  deployer.deploy(Token);
+  deployer.deploy(Controller).then(() => {
+    return deployer.deploy(DefaultSweeper, Controller.address);
+  });
+};
